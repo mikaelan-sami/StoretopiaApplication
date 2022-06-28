8683b030f6d5ef3e679fc24806f40543658b26b7
mikae <mikaelan.sami@gmail.com>
2022-06-28 07:21:14 +0200

    Code to display the pie chart added

--- app/src/main/java/com/example/storetopiaapplication/displayListView.java
+++ app/src/main/java/com/example/storetopiaapplication/displayListView.java
@@ -3,6 +3,7 @@ package com.example.storetopiaapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,6 +11,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,6 +22,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class displayListView extends AppCompatActivity {
     private FirebaseDatabase mFirebaseDatabase;
@@ -26,6 +33,8 @@ public class displayListView extends AppCompatActivity {
     public static String plName, plTeam, plDate;
     private ArrayAdapter<String> categoryArrayAdapter;
     CreateCollections cc = new CreateCollections();
+    categoryView cv = new categoryView();
+    PieChart pc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +51,8 @@ public class displayListView extends AppCompatActivity {
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int count = (int) snapshot.getChildrenCount();
+                displayPieChart(count);
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()){
                      plDate = dataSnapshot.child("playerTeam").getValue().toString();
                      plName = dataSnapshot.child("playerName").getValue().toString();
@@ -72,6 +83,40 @@ public class displayListView extends AppCompatActivity {
                 Toast.makeText(displayListView.this, "This item is called" + categoryArrayAdapter.getItem(i), Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    private void displayPieChart(int count) {
+
+        String label = "type";
+
+        int goalAmount = Integer.parseInt(cc.goals);
+        int remainingAmount = goalAmount - count;
+
+        ArrayList<PieEntry> pieEntries = new ArrayList<>();
+
+        Map<String, Integer> typeAmountMap = new HashMap<>();
+
+        typeAmountMap.put("Cards Collected", count);
+        typeAmountMap.put("Cards to Acquire", remainingAmount);
+
+        ArrayList<Integer> colours = new ArrayList<>();
+        colours.add(Color.parseColor("#0f02f7"));
+        colours.add(Color.parseColor("#ef02f7"));
+
+        for (String type: typeAmountMap.keySet())
+        {
+            pieEntries.add(new PieEntry(typeAmountMap.get(type).floatValue(), type));
+        }
+
+        PieDataSet pds = new PieDataSet(pieEntries, label);
+        pds.setValueTextSize(10f);
+        pds.setColors(colours);
+        PieData pd = new PieData(pds);
+        pd.setDrawValues(true);
+
+        pc.setData(pd);
+        pc.invalidate();
 
     }
 }
